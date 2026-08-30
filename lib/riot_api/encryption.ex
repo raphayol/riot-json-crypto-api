a022976555348617787a9c5b5c3565d39357c7d0
Egeley Raphael <raphael.egeley@gmail.com>
2026-08-31 00:14:27 +0700

    feat: orchestrate payload encryption

---
+++ lib/riot_api/encryption.ex
@@ -0,0 +1,15 @@
+defmodule RiotApi.Encryption do
+  alias RiotApi.Payload
+
+  def encrypt(payload, cipher) do
+    Payload.map_depth_one(payload, fn value ->
+      cipher.encrypt(value)
+    end)
+  end
+
+  def decrypt(payload, cipher) do
+    Payload.map_depth_one(payload, fn value ->
+      cipher.decrypt(value)
+    end)
+  end
+end
